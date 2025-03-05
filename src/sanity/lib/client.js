a694e533be3767821f4d7086b14d1caf5dbd6763
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-03-06 00:41:31 +0500

    Update client.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,17 +1,17 @@
 import sanityClient from '@sanity/client';
 
 const projectId = process.env.SANITY_PROJECT_ID;
-const dataset = process.env.SANITY_DATASET;
+const dataset = process.env.SANITY_DATASET || "production";
 
 if (!projectId) {
-  throw new Error('Sanity projectId is missing. Please set the SANITY_PROJECT_ID environment variable.');
+  throw new Error("Sanity projectId is missing. Please set the SANITY_PROJECT_ID environment variable.");
 }
 if (!dataset) {
-  throw new Error('Sanity dataset is missing. Please set the SANITY_DATASET environment variable.');
+  throw new Error("Sanity dataset is missing. Please set the SANITY_DATASET environment variable.");
 }
 
 export const client = sanityClient({
   projectId,
   dataset,
-  useCdn: true, // Set to false if you need fresh data instead of cached
+  useCdn: true, // Set to false for fresh data
 });

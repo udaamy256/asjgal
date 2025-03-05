7439408492ed7e3a0df304b374e5f9a3c8969060
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-03-06 01:29:59 +0500

    Update client.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,10 +1,8 @@
-import { createClient } from 'next-sanity'
+import sanityClient from '@sanity/client';
 
-import { apiVersion, dataset, projectId } from '../env'
-
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+export const client = sanityClient({
+  projectId: process.env.SANITY_PROJECT_ID, // Load from environment variable
+  dataset: 'production',                   // Replace with your dataset name
+  apiVersion: '2023-10-01',               // Specify an API version (e.g., today's date)
+  useCdn: true                            // Set to false if you need real-time data
+});

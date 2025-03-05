3605a5f3e34a7a494b1fd2aa50fcfd29d54e4746
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-03-06 01:48:19 +0500

    Update env.js

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,4 +1,6 @@
-SANITY_PROJECT_ID="g9webycc"
-SANITY_DATASET="production"
-NEXT_PUBLIC_SANITY_PROJECT_ID="g9webycc"
-NEXT_PUBLIC_SANITY_DATASET="production"
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-09-21'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;

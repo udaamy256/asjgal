3168c7fba62c9984d157aec8e2c3cf478d4a4c97
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-03-06 16:09:27 +0500

    Update client.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,8 +1,10 @@
 import { createClient } from '@sanity/client';
 
-const client = createClient({
-  projectId: 'g9webycc',  // Replace this with your actual projectId
-  dataset: 'production',         // Replace with your dataset, typically 'production'
-  apiVersion: '2023-03-01',      // Specify an API version (recommended)
-  useCdn: true,                  // Use CDN for faster responses (set to false if you need the latest data)
-});
+import { apiVersion, dataset, projectId } from '../env'
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+})

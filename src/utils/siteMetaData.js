9c92d17027b7eede334fc5a611fc896d6abdfb32
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-03-12 15:39:09 +0500

    Update siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,17 +1,17 @@
 const siteMetadata = {
-  title: 'courses',
-  author: 'Epics',
+  title: 'Study Visa Consultant',
+  author: 'Galaxcy',
   headerTitle: 'Blog',
-  description: 'Educational websites help students gain enroll in the best courses.',
+  description: 'Educational websites help students gain admission to top universities and enroll in the best courses.',
   language: 'en-us',
   theme: 'dark', // system, dark or light
-  siteUrl: 'https://www.epicssolution.com/', // your website URL
+  siteUrl: 'https://www.galaxyeducation.org/', // your website URL
   siteLogo: '/logo.png',
   email: 'galaxyconsultant25600@gmail.com',
-  facebook: 'https://www.facebook.com/EPICS256',
-  youtube: 'https://www.youtube.com/@Dharti-256',
-  linkedin: 'https://www.linkedin.com/company/mhp-design-engineering',
-  instagram: 'https://www.linkedin.com/company/mhp-design-engineering',
+  facebook: 'https://www.facebook.com/GalaxyAbraodEducation',
+  youtube: 'https://www.youtube.com/@Educationconsultant-o2j',
+  linkedin: 'http://linkedin.com/in/galaxy-abroadeducation-009a04205',
+  instagram: 'https://www.instagram.com/galaxyconsultancy2222',
   locale: 'en-US',
 
   fbAppID: 'your-facebook-app-id', 

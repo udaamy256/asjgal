a5a8086ff4ad4707c67970dd7cc019b8dca96168
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-04-24 04:40:23 -0700

    Update page.js

--- src/components/bloglayoutone/unicomponent/page.js
+++ src/components/bloglayoutone/unicomponent/page.js
@@ -86,7 +86,7 @@ const UniComponent1  = () => {
                 )}
                 <div className="flex flex-col md:flex-row gap-4">
                   <div className="md:w-1/3">
-                    <Link href={`/blog/${post.slug}`}>
+                    <Link href={`/university/${post.slug}`}>
                       <div className="relative w-full pt-[75%]">
                         <Image
                           src={urlFor(post.image).url()}
@@ -113,7 +113,7 @@ const UniComponent1  = () => {
                       })}
                     </span>
                     <p className="mt-2 text-gray-700">{post.description}</p>
-                    <Link href={`/blog/${post.slug}`}>
+                    <Link href={`/university/${post.slug}`}>
                       <button className="mt-4 px-4 py-2 bg-[#FF6F61] text-white rounded hover:bg-[#E65C50]">
                         Read More
                       </button>
@@ -154,7 +154,7 @@ const UniComponent1  = () => {
                     />
                   </div>
                   <div className="ml-4">
-                    <Link href={`/blog/${post.slug}`}>
+                    <Link href={`/university/${post.slug}`}>
                       <h4 className="text-sm font-medium hover:underline text-gray-900 dark:text-gray-100">
                         {post.title.length > 20
                           ? `${post.title.slice(0, 20)}...`

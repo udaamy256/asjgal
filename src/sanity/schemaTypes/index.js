20fba8b7e0c80770037cf41ccc68be839f6144a4
udaamy256 <71670853+udaamy256@users.noreply.github.com>
2025-05-15 07:11:40 -0700

    Update index.js

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -3,7 +3,9 @@ import { blogType } from "../blogType"
 import { secType } from "../secType"
 import { uniType } from "../uniType"
 import { sucessType } from "../sucesstype"
+import { germanyType } from "../germanyType"
+
 
 export const schema = {
-  types: [eventType, blogType, secType, uniType, sucessType],
+  types: [eventType, blogType, secType, uniType, sucessType,germanyType],
 }
